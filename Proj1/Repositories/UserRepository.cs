23f5fd173c4c41f48688e3be1c1512c5e5743cd9
Xondamir <xondamiraxmadjonov2002@gmail.com>
2023-04-16 07:01:15 +0500

    UpdateUserPhoto method added

--- Proj1/Repositories/UserRepository.cs
+++ Proj1/Repositories/UserRepository.cs
@@ -122,16 +122,16 @@ namespace Proj1.Repositories
             command.ExecuteNonQuery();
         }
 
-        //public void UpdateUserPhoto(string photoPath,User user)
-        //{
-        //    user.PhotoPath = photoPath;
-        //    var command = _connection.CreateCommand();
-        //    command.CommandText = "UPDATE users SET photo_url = @photo_url WHERE id=@id";
-        //    command.Parameters.AddWithValue("photo_url", user.PhotoPath);
-        //    command.Parameters.AddWithValue("id", user.Id);
-        //    command.Prepare();
-        //    command.ExecuteNonQuery();
-        //}
+        public void UpdateUserPhoto(string photoPath, User user)
+        {
+            user.PhotoPath = photoPath;
+            var command = _connection.CreateCommand();
+            command.CommandText = "UPDATE users SET photo_url = @photo_url WHERE id=@id";
+            command.Parameters.AddWithValue("photo_url", user.PhotoPath);
+            command.Parameters.AddWithValue("id", user.Id);
+            command.Prepare();
+            command.ExecuteNonQuery();
+        }
 
         //public void UpdateUserPassword(ChangeUserModel changeUserModel, User user)
         //{
